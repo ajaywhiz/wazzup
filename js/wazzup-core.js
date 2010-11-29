23dd949126abd2428fdace0f84f71db267caa864
ajaywhiz <ajay_whiz@rediffmail.com>
2010-11-29 12:18:26 +0530

    initial core

---
+++ js/wazzup-core.js
@@ -0,0 +1,15 @@
+wazzup = function (location) {
+
+    var _location = location;
+    var _services = [];
+    this.start = start;
+
+    function start() {
+
+    }
+
+    function registerService(service) {
+        _services.push(service);
+    }
+
+} 
